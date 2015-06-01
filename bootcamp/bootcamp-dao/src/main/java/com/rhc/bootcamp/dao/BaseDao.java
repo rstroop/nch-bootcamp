90b01dc099d239690b4a03261f6201d42ef3b490
Justin Cohler <justincohler@gmail.com>
2015-06-01 09:57:05 -0400

    added beginnings of maven structure

---
+++ bootcamp/bootcamp-dao/src/main/java/com/rhc/bootcamp/dao/BaseDao.java
@@ -0,0 +1,7 @@
+package com.rhc.bootcamp.dao;
+
+public interface BaseDao {
+    
+    // TODO -- lookup Mongo ORM for Java
+    
+}
---
+++ bootcamp/bootcamp-dao/src/main/java/com/rhc/bootcamp/dao/BaseMongoDao.java
@@ -0,0 +1,24 @@
+package com.rhc.bootcamp.dao;
+
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+
+public class BaseMongoDao {
+
+    private MongoClient mongoClient;
+    private DB db;
+    
+    public MongoClient getMongoClient() {
+        return mongoClient;
+    }
+    public void setMongoClient(MongoClient mongoClient) {
+        this.mongoClient = mongoClient;
+    }
+    public DB getDb() {
+        return db;
+    }
+    public void setDb(DB db) {
+        this.db = db;
+    }
+
+}
