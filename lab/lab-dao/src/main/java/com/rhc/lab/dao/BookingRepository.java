044a7e013ec0202dea7cf430690960e102a9cbe2
Justin Cohler <justincohler@gmail.com>
2015-06-17 15:47:34 -0400

    updated dao repos with javadocs

--- lab/lab-dao/src/main/java/com/rhc/lab/dao/BookingRepository.java
+++ lab/lab-dao/src/main/java/com/rhc/lab/dao/BookingRepository.java
@@ -5,6 +5,13 @@ import org.springframework.stereotype.Repository;
 
 import com.rhc.lab.domain.BookingRequest;
 
+/**
+ * 
+ * This interface extends the Spring-Data-MongoDB interface, exposing base
+ * MongoDB operations and additional field level queries for the bookings
+ * collection
+ * 
+ */
 @Repository
 public interface BookingRepository
 		extends
--- lab/lab-dao/src/main/java/com/rhc/lab/dao/VenueRepository.java
+++ lab/lab-dao/src/main/java/com/rhc/lab/dao/VenueRepository.java
@@ -5,6 +5,13 @@ import org.springframework.stereotype.Repository;
 
 import com.rhc.lab.domain.Venue;
 
+/**
+ * 
+ * This interface extends the Spring-Data-MongoDB interface, exposing base
+ * MongoDB operations and additional field level queries for the venues
+ * collection
+ * 
+ */
 @Repository
 public interface VenueRepository
 		extends
