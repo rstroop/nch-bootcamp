19714d4d6c3b06da9fdd54f6bfba3f89d663f00e
Cade Friedenbach <cadenfriedenbach@gmail.com>
2015-06-30 19:19:13 -0400

    fixed unit test in kie-services

--- lab/lab-kie-services/src/test/java/com/rhc/lab/drools/DroolsTest.java
+++ lab/lab-kie-services/src/test/java/com/rhc/lab/drools/DroolsTest.java
@@ -3,22 +3,28 @@ package com.rhc.lab.drools;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.Resource;
+
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.BootstrapWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.rhc.lab.domain.BookingRequest;
 import com.rhc.lab.kie.api.StatelessDecisionService;
 import com.rhc.lab.kie.service.impl.LocalStatelessDecisionService;
 
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(locations= {"classpath:kie-context.xml"} )
+@RunWith(SpringJUnit4ClassRunner.class)
+@BootstrapWith()
+@ContextConfiguration(locations = {"classpath:kie-context.xml"})
 public class DroolsTest {
-	// TODO: Figured out why junit is angry here
-	// @Resource(name ="localDecisionService")
+	@Resource(name = "localDecisionService")
 	private StatelessDecisionService decisionService;
 
-	@Ignore
+
 	@Test
 	public void shouldAutowireDecisionService() {
 		Assert.assertNotNull(decisionService);
@@ -29,13 +35,10 @@ public class DroolsTest {
 
 		BookingRequest request = new BookingRequest();
 
-		LocalStatelessDecisionService service = new LocalStatelessDecisionService(
-				"kbase");
-
 		List<Object> facts = new ArrayList<Object>();
 		facts.add(request);
 
-		service.execute(facts);
+		decisionService.execute(facts);
 
 	}
 
